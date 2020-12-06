457606e92211859c39a530fc0181bd4f6d1d3b65
jessica <jessicap@adblockplus.org>
2020-12-06 20:57:18 +0100

    add code for advent 6

--- adventofcode/advent6.py
+++ adventofcode/advent6.py
@@ -1,10 +1,56 @@
-def group_questions(group_entries: list[str]) -> set[str]:
-    pass
+import typing
+from collections import Counter
+
+
+def load_input(user_data_file):
+    with open(user_data_file, "r") as fd:
+        for line in fd:
+            yield line.strip()
+
+
+def read_groups(lines: typing.Iterable[str]) -> typing.Iterable[list]:
+    result = []
+    for line in lines:
+        if line == "":
+            yield result
+            result = []
+            continue
+        else:
+            result.append(line)
+    if result:
+        yield result
+
+
+def group_questions(group_entries: list[str]) -> dict[str, int]:
+    """
+    Receives the answers from the whole group, and returns, for each
+    question, how many in the group answered yes
+    """
+    result = []
+    for entry in group_entries:
+        for answer in entry:
+            result.append(answer)
+    result_dict = Counter(result)
+    return result_dict
 
 
 def main1(input_file: str) -> int:
-    pass
+    group_entries = load_input(input_file)
+    groups = read_groups(group_entries)
+    count = 0
+    for group in groups:
+        unique_answers = group_questions(group)
+        count += len(unique_answers)
+    return count
 
 
 def main2(input_file: str):
-    pass
+    group_entries = load_input(input_file)
+    groups = read_groups(group_entries)
+    count = 0
+    for group in groups:
+        answers = group_questions(group)
+        for answer, times in answers.items():
+            if times == len(group):
+                count += 1
+    return count
--- tests/test_advent6.py
+++ tests/test_advent6.py
@@ -1,7 +1,6 @@
 import os.path
 
 import pytest
-
 from adventofcode import advent6
 
 
@@ -10,17 +9,17 @@ def test_advent6(user, user_data, record_testsuite_property):
     result1 = advent6.main1(file)
     record_testsuite_property(f"{user}-6-1", result1)
     result2 = advent6.main2(file)
-    record_testsuite_property(f"{user}-1-2", result2)
+    record_testsuite_property(f"{user}-6-2", result2)
 
 
 @pytest.mark.parametrize(
     "input,expected",
     (
-        pytest.param(["abc"], {"a", "b", "c"}, id="example1"),
-        pytest.param(["a", "b", "c"], {"a", "b", "c"}, id="example2"),
-        pytest.param(["ab", "ac"], {"a", "b", "c"}, id="example3"),
-        pytest.param(["a", "a", "a", "a"], {"a"}, id="example4"),
-        pytest.param(["b"], {"b"}, id="example4"),
+            pytest.param(["abc"], {"a": 1, "b": 1, "c": 1}, id="example1"),
+            pytest.param(["a", "b", "c"], {"a": 1, "b": 1, "c": 1}, id="example2"),
+            pytest.param(["ab", "ac"], {"a": 2, "b": 1, "c": 1}, id="example3"),
+            pytest.param(["a", "a", "a", "a"], {"a": 4}, id="example4"),
+            pytest.param(["b"], {"b": 1}, id="example5"),
     ),
 )
 def test_group_questions(input, expected):
