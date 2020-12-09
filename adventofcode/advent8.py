382cabe537d8367f91a1e78b7a08c5904e59c152
jessica <jessicap@adblockplus.org>
2020-12-09 09:37:13 +0100

    add advent8 files

--- adventofcode/advent8.py
+++ adventofcode/advent8.py
@@ -1,3 +1,4 @@
+import dataclasses
 
 
 def load_input(user_data_file):
@@ -5,44 +6,55 @@ def load_input(user_data_file):
         for line in fd:
             yield line.strip()
 
-def get_instruction(line):
-    command, _, times = line.partition(" ")
-    return {command, int(times)}
-
-def acc(position, accumulator, time):
-    position += 1
-    accumulator = accumulator + time
-    return {position, accumulator}
 
 
-def nop(position, accumulator):
-    position += 1
-    return {position, accumulator}
+def acc(emulator):
+    _, _, value = emulator.program[emulator.program_counter].partition(" ")
+    emulator.program_counter += 1
+    emulator.accumulator += int(value)
 
 
-def jmp(position, accumulator, time):
-    position = position + time
-    return {position, accumulator}
+def nop(emulator):
+    emulator.program_counter += 1
+
+
+def jmp(emulator):
+    _, _, value = emulator.program[emulator.program_counter].partition(" ")
+    emulator.program_counter += int(value)
+
+
+@dataclasses.dataclass
+class Emulator:
+    accumulator: int
+    program: list[str]
+    program_counter: int
+
+    def execute_once(self):
+        line = self.program[self.program_counter]
+        if line.startswith("acc "):
+            acc(self)
+        elif line.startswith("nop "):
+            nop(self)
+        elif line.startswith("jmp "):
+            jmp(self)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.execute_once()
+        return self
+
 
 def main1(input_file):
     lines = load_input(input_file)
-    position = 0
-    accumulator = 0
-    been_there = []
-    while position not in been_there:
-        been_there.append(position)
-        for line in lines:
-            command, time = get_instruction(line)
-            if command == "acc":
-                position, accumulator = acc(position, accumulator, time)
-                been_there.append(position)
-            if command == "nop":
-                position, accumulator = nop(position, accumulator)
-                been_there.append(position)
-            if command == "jmp":
-                position, accumulator = jmp(position, accumulator, time)
-                been_there.append(position)
-    return {"position": position, "accumulator": accumulator}
+    been_there = {0}
+    emulator = Emulator(accumulator = 0, program = list(lines), program_counter = 0)
+    for _ in emulator:
+        if emulator.program_counter in been_there:
+            break
+        been_there.add(emulator.program_counter)
+    return emulator.accumulator
 
-def main2():
+def main2(input_file):
     pass
--- tests/test_advent8.py
+++ tests/test_advent8.py
@@ -1,23 +1,32 @@
 import os
 
 import pytest
-
 from adventofcode import advent8
 
 
 def test_advent8(user, user_data, record_testsuite_property):
-    file = os.path.join(user_data, "7.txt")
+    file = os.path.join(user_data, "8.txt")
     result = advent8.main1(file)
-    record_testsuite_property(f"{user}-7-1", result)
+    record_testsuite_property(f"{user}-8-1", result)
     result = advent8.main2(file)
-    record_testsuite_property(f"{user}-7-2", result)
+    record_testsuite_property(f"{user}-8-2", result)
 
 
 @pytest.mark.parametrize(
-    "input,expected",
+    "input,execution_order,accumulator",
     (
-            pytest.param(("nop +0", "acc +1", "jmp +4", "acc +3", "jmp -3", "acc -99", "acc +1", "jmp -4", "acc +6"),
-            {"position": 1, "accumulator": 5})))
+            pytest.param(
+                ["nop +0", "acc +1", "jmp +4", "acc +3", "jmp -3", "acc -99", "acc +1", "jmp -4", "acc +6"],
+                [0, 1, 2, 6, 7, 3, 4, 1],
+                5,
+                id="example1"),
+    ))
+def test_execute_once(input, execution_order, accumulator):
+    emulator = advent8.Emulator(accumulator=0, program=input, program_counter=0)
 
-def test_main1(input, expected):
-    assert advent8.main1(input) == expected
+    assert emulator.program_counter == execution_order.pop(0)
+
+    for expected_program_counter, _ in zip(execution_order, emulator):
+        assert emulator.program_counter == expected_program_counter
+
+    assert emulator.accumulator == accumulator
